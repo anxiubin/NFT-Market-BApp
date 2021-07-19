1cdac6bb95a22c876c0bf092cb10b9d5a5eb1dc1
anxiubin <tnqls0120@naver.com>
2021-07-19 23:02:37 +0900

    ♻️ remove useless files

--- src/abi/SetCountABI.js
+++
@@ -1,2 +0,0 @@
-export const SET_COUNT_ABI =
-	'{ "constant": false, "inputs": [ { "name": "_count", "type": "uint256" } ], "name": "setCount", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }'
