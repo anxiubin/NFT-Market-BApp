6c4b0a8d96a471252c5283bd0efc35573b8d2de5
anxiubin <tnqls0120@naver.com>
2021-07-21 20:41:10 +0900

    ✨ add features for NFT and Klip
    - mint Token
    - buy Token
    - list NFT on Market
    - get NFT lists

--- src/api/UseCaver.js
+++ src/api/UseCaver.js
@@ -1,11 +1,9 @@
 import Caver from "caver-js"
-import CounterABI from "../abi/CounterABI.json"
-import AirdndABI from "../abi/AirdndABI.json"
+import KIP17ABI from "../abi/KIP17TokenABI.json";
 import {
 	ACCESS_KEY_ID,
 	SECRET_ACCESS_KEY,
-	AIRDND_CONTRACT_ADDRESS,
-	DEPLOY_ADDRESS_PRIVATE_KEY,
+	NFT_CONTRACT_ADDRESS,
 	CHAIN_ID,
 } from "../config"
 
@@ -29,24 +27,63 @@ const caver = new Caver(
 		option
 	)
 )
+
 // 참조 ABI와 스마트컨트랙트 주소를 통해 스마트컨트랙트 연동
-const AirdndContract = new caver.contract(AirdndABI, AIRDND_CONTRACT_ADDRESS)
+const NFTContract = new caver.contract(KIP17ABI, NFT_CONTRACT_ADDRESS);
+
+export const fetchCardsOf = async (address) => {
+	/* 
+		NFT 가져오는 방법 (1)
+		1. balanceOf 함수 를 사용해서 전체 NFT 개수를 가져온다
+		2. 전체 NFT 개수만큼 반복문을 돌면서 tokenOfOwnerByIndex 함수를 이용하여 tokenId를 하나씩 가져오고 tokenIds 배열에 담는다
+		3. tokenURI함수 를 이용해 앞에서 담아둔 tokenIds 배열을 돌면서 tokenURI를 하나씩 가져온다 
+
+		NFT 가져오는 방법 (2)
+		KAS API 중에 Token 관련 API 사용
+		참고: https://refs.klaytnapi.com/ko/th/latest#operation/getNftsByOwnerAddress
+	*/
+
+  // Fetch Balance
+  const balance = await NFTContract.methods.balanceOf(address).call();
+  console.log(`[NFT Balance]${balance}`);
+  // Fetch Token IDs
+  const tokenIds = [];
+  for (let i = 0; i < balance; i++) {
+    const id = await NFTContract.methods.tokenOfOwnerByIndex(address, i).call();
+    tokenIds.push(id);
+  }
+  // Fetch Token URIs
+  const tokenUris = [];
+  for (let i = 0; i < balance; i++) {
+    const uri = await NFTContract.methods.tokenURI(tokenIds[i]).call();
+    tokenUris.push(uri);
+  }
+  const nfts = [];
+  for (let i = 0; i < balance; i++) {
+    nfts.push({ uri: tokenUris[i], id: tokenIds[i] });
+  }
+  console.log(nfts);
+  return nfts;
+};
+
+// 클레이 지갑의 잔고 조회하기
+export const getBalance = (address) => {
+  return caver.rpc.klay.getBalance(address).then((response) => {
+		//16진수 response를 숫자로 바꾸고, PEB단위에서 KLAY단위로 변환
+    const balance = caver.utils.convertFromPeb(
+      caver.utils.hexToNumberString(response)
+    );
+    console.log(`BALANCE: ${balance}`);
+    return balance;
+  });
+};
+
+
+/*
 
 export const readOwner = async () => {
 	const _owner = await AirdndContract.methods.owner().call()
 	console.log(_owner)
-}
-
-// 클레이 지갑의 잔고 조회하기
-export const getBalance = (address) => {
-	return caver.rpc.klay.getBalance(address).then((response) => {
-		const balance = caver.utils.convertFromPeb(
-			caver.utils.hexToNumberString(response)
-		) //16진수 response를 숫자로 바꾸고, PEB단위에서 KLAY단위로 변환
-
-		console.log(`BALANCE: ${balance}`)
-		return balance
-	})
 }
 
 //스마트컨트랙트 실행하기
@@ -87,3 +124,5 @@ export const getRoomsByHost = async (hostId) => {
 		console.log(`[ERROR_getRoomsByHost]${e}`)
 	}
 }
+
+*/
--- src/api/UseKlip.js
+++ src/api/UseKlip.js
@@ -1,8 +1,9 @@
-import axios from "axios"
-import { AIRDND_CONTRACT_ADDRESS } from "../config"
-import { A2P_API_PREPARE_URL, APP_NAME } from "../constants"
-import { SET_COUNT_ABI } from "../abi/SetCountABI"
-import { BOOKING_ROOM_ABI } from "../abi/BookingRoomABI"
+import axios from "axios";
+import {
+	NFT_CONTRACT_ADDRESS,
+	MARKET_CONTRACT_ADDRESS,
+} from "../config"
+
 /*
   KLIP API : PREPARE (지갑 사용 여부 체크) => REQUEST(컨트랙트 실행) => RESULT(결과 체크)
 	(참고: https://docs.klipwallet.com/rest-api/rest-api-a2a)
@@ -10,6 +11,149 @@ import { BOOKING_ROOM_ABI } from "../abi/BookingRoomABI"
 	App2App 처리 시 가정 먼저 수행해야할 Prepare 과정은 Klip에서 처리할 요청 데이터를 전달하고 Request Key를 발급받는 과정입니다. 
 	Reqeust Key는 Deep Link 호출 및 QR code 결과 확인 과정에서 필요합니다.
 */
+
+const A2P_API_PREPARE_URL = "https://a2a-api.klipwallet.com/v2/a2a/prepare";
+const APP_NAME = "KLAY_MARKET";
+const isMobile = /iPhone|iPad|iPod|Android|webOS|BlackBerry|Windows Phone/i.test(window.navigator.userAgent)
+
+const getKlipAccessUrl = (method, request_key) => {
+  if (method === "QR") {
+    return `https://klipwallet.com/?target=/a2a?request_key=${request_key}`;
+  }
+  return `kakaotalk://klipwallet/open?url=https://klipwallet.com/?target=/a2a?request_key=${request_key}`;
+};
+export const buyCard = async (tokenId, setQrvalue, callback) => {
+  const functionJson =
+    '{ "constant": false, "inputs": [ { "name": "tokenId", "type": "uint256" }, { "name": "NFTAddress", "type": "address" } ], "name": "buyNFT", "outputs": [ { "name": "", "type": "bool" } ], "payable": true, "stateMutability": "payable", "type": "function" }';
+  executeContract(
+    MARKET_CONTRACT_ADDRESS,
+    functionJson,
+    "10000000000000000",
+    `[\"${tokenId}\",\"${NFT_CONTRACT_ADDRESS}\"]`,
+    setQrvalue,
+    callback
+  );
+};
+
+export const listingCard = async (
+  fromAddress,
+  tokenId,
+  setQrvalue,
+  callback
+) => {
+  const functionJson =
+    '{ "constant": false, "inputs": [ { "name": "from", "type": "address" }, { "name": "to", "type": "address" }, { "name": "tokenId", "type": "uint256" } ], "name": "safeTransferFrom", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }';
+  executeContract(
+    NFT_CONTRACT_ADDRESS,
+    functionJson,
+    "0",
+    `[\"${fromAddress}\",\"${MARKET_CONTRACT_ADDRESS}\",\"${tokenId}\"]`,
+    setQrvalue,
+    callback
+  );
+};
+
+export const mintCardWithURI = async (
+  toAddress,
+  tokenId,
+  uri,
+  setQrvalue,
+  callback
+) => {
+  const functionJson =
+    '{ "constant": false, "inputs": [ { "name": "to", "type": "address" }, { "name": "tokenId", "type": "uint256" }, { "name": "tokenURI", "type": "string" } ], "name": "mintWithTokenURI", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }';
+  executeContract(
+    NFT_CONTRACT_ADDRESS,
+    functionJson,
+    "0",
+    `[\"${toAddress}\",\"${tokenId}\",\"${uri}\"]`,
+    setQrvalue,
+    callback
+  );
+};
+
+export const executeContract = (
+  txTo,
+  functionJSON,
+  value,
+  params,
+  setQrvalue,
+  callback
+) => {
+  axios
+    .post(A2P_API_PREPARE_URL, {
+      bapp: {
+        name: APP_NAME,
+      },
+      type: "execute_contract",
+      transaction: {
+        to: txTo,
+        abi: functionJSON,
+        value: value,
+        params: params,
+      },
+    })
+    .then((response) => {
+      const { request_key } = response.data;
+      if (isMobile) {
+        window.location.href = getKlipAccessUrl("deeplink", request_key);
+      } else {
+        setQrvalue(getKlipAccessUrl("QR", request_key));
+      }
+
+      let timerId = setInterval(() => {
+        axios
+          .get(
+            `https://a2a-api.klipwallet.com/v2/a2a/result?request_key=${request_key}`
+          )
+          .then((res) => {
+            if (res.data.result) {
+              console.log(`[Result] ${JSON.stringify(res.data.result)}`);
+              callback(res.data.result);
+              clearInterval(timerId);
+              setQrvalue("DEFAULT");
+            }
+          });
+      }, 1000);
+    });
+};
+
+
+// QR code 또는 Deep Link 활용해서 Klip 주소값 가져오기
+export const getAddress = (setQrvalue, callback) => {
+  axios
+    .post(A2P_API_PREPARE_URL, {
+      bapp: {
+        name: APP_NAME,
+      },
+      type: "auth",
+    })
+    .then((response) => {
+      const { request_key } = response.data;
+      if (isMobile) {
+        window.location.href = getKlipAccessUrl("deeplink", request_key);
+      } else {
+        setQrvalue(getKlipAccessUrl("QR", request_key));
+      }
+      let timerId = setInterval(() => {
+        axios
+          .get(
+            `https://a2a-api.klipwallet.com/v2/a2a/result?request_key=${request_key}`
+          )
+          .then((res) => {
+            if (res.data.result) {
+              console.log(`[Result] ${JSON.stringify(res.data.result)}`);
+              callback(res.data.result.klaytn_address);
+              clearInterval(timerId);
+              setQrvalue("DEFAULT");
+            }
+          });
+      }, 1000);
+    });
+};
+
+
+/*
 
 export const bookRoom = (hostId, index, date, setQrvalue) => {
 	axios
@@ -53,33 +197,4 @@ export const bookRoom = (hostId, index, date, setQrvalue) => {
 			}, 1000)
 		})
 }
-
-// QR code 활용해서 Klip 주소값 가져오기
-export const getAddress = (setQrvalue, setAddress) => {
-	axios
-		.post(A2P_API_PREPARE_URL, {
-			bapp: {
-				name: APP_NAME,
-			},
-			type: "auth",
-		})
-		.then((response) => {
-			const { request_key } = response.data //Reqeust Key 발급 성공
-			const qrcode = `https://klipwallet.com/?target=/a2a?request_key=${request_key}`
-			setQrvalue(qrcode) //Klip에 인증 또는 서명을 요청하기 위해 QR code 방식 사용
-
-			// 결과값 가져오기
-			let timerId = setInterval(() => {
-				axios
-					.get(
-						`https://a2a-api.klipwallet.com/v2/a2a/result?request_key=${request_key}`
-					)
-					.then((res) => {
-						if (res.data.result) {
-							setAddress(res.data.result.klaytn_address)
-							clearInterval(timerId) // 결과값 가져오기 성공하면 setInterval 실행 중단
-						}
-					})
-			}, 1000)
-		})
-}
+*/
